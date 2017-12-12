a5b5e34578b3e092e3f057ed01e3ed205ea49121
Przemyslaw Misztal <przemyslaw.misztal@atos.net>
2017-12-12 14:13:37 +0100

    solution init

---
+++ MyWeddingWebsite/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyWeddingWebsite
+{
+	[Route("api/[controller]")]
+	public class ValuesController : Controller
+	{
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "Hello", "World" };
+		}
+	}
+}
